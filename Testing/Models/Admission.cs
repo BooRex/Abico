2936403082e577d84cf82870ff20f7964f45e24b
Alex <Kolesni2Oleksii@m.hneu.edu.ua>
2018-04-15 16:35:17 +0300

    Added classes Admission, College, Enrollee

---
+++ Testing/Models/Admission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class Admission
+    {
+        public int ID { get; set; }
+        public virtual Enrollee Enrollee { get; set; }
+        public bool Budget { get; set; }
+        public bool Daytime { get; set; }
+    }
+}
---
+++ Testing/Models/Enrollee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class Enrollee
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Phone { get; set; }
+        public decimal CertAv { get; set; }
+        public virtual College College { get; set; }
+        public virtual ICollection<Submission> Submissions { get; set; }
+        public virtual ICollection<Admission> Admissions { get; set; }
+        public virtual ICollection<Test> Tests { get; set; }
+    }
+}
