7f775050585395a442b763665f6ec39a5f523b0b
Alex <Kolesni2Oleksii@m.hneu.edu.ua>
2018-04-15 16:47:54 +0300

    Add class TextContent

---
+++ Testing/DataAccess/TestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Testing.Models;
+
+namespace Testing.DataAccess
+{
+    class TestContext : DbContext
+    {
+        public DbSet<Enrollee> Enrollees { get; set; }
+        public DbSet<College> Colleges { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Admission> Admissions { get; set; }    }
+}
